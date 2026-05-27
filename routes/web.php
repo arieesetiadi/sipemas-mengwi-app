2fda1f968d44cc8cbae809eb97b78daf89aac34c
Arie Setiadi <ariee.setiadi@gmail.com>
2026-05-27 16:19:47 +0800

    prepare template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Portal\HomeController;
+use App\Http\Controllers\System\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+// PORTAL (Masyarakat)
+Route::as('portal.')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+});
+
+// ---------------------------------
+
+// SYSTEM (Admin/Internal)
+Route::prefix('system')->as('system.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
